3a48c65d939585a573a94c493489b0f88499882b
anoop-k-n <anoopkn2001@pm.me>
2022-11-14 00:15:03 +0530

    Setup navigation bar layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useScroll } from "framer-motion";
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar"
 
 function App() {
-  return (
-    <div className="App">
+  const [selectedPage,setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1600px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true); 
 
+  useEffect(() => {
+    const handlescroll = () => {
+      if (window.screenY === 0) setIsTopOfPage(true);
+      else setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handlescroll);
+    return () => window.removeEventListener("scroll", handlescroll);
+  },[])
+
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar 
+        isTopOfPage= {isTopOfPage}
+        selectedPage = {selectedPage}
+        setSelectedPage = {setSelectedPage}
+      />
+      
     </div>
   );
 }
