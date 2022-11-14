fc139c57dad203188596f8073b4990daa2007c9c
anoop-k-n <anoopkn2001@pm.me>
2022-11-15 01:47:39 +0530

    Finished final setup and removed some assets

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -3,31 +3,16 @@ const SocialMediaIcons = () => {
     <div className="flex justify-center md:justify-start my-10 gap-7">
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.linkedin.com"
+        href="https://www.linkedin.com/in/anoop-k-n-a369a71a2/"
         target="_blank"
         rel="noreferrer"
       >
         <img alt="linkedin-link" src="../assets/linkedin.png" />
       </a>
+
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.twitter.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img alt="twitter-link" src="../assets/twitter.png" />
-      </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
-        href="https://www.facebook.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img alt="facebook-link" src="../assets/facebook.png" />
-      </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
-        href="https://www.instagram.com"
+        href="https://www.instagram.com/anoop.k_n/"
         target="_blank"
         rel="noreferrer"
       >
